21a19af1b8de557a6295bfcdf844cfd8d2b9aa53
JxJ454 <122534317+JxJ454@users.noreply.github.com>
2023-06-23 10:19:00 +0200

    Add files via upload
    some variables use diff values

--- assignment2/agents.py
+++ assignment2/agents.py
@@ -14,10 +14,11 @@ TIMESTEP_INTERVAL = 6  # 0.1 seconds (assuming 60fps)
 
 
 class MyBaseAgent(Agent):
-    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None, move: Vector2 | None = None):
+    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None,
+                 move: Vector2 | None = None):
         super().__init__(images, simulation, pos, move)
         self.simulation = simulation
-    
+
     # Basic random movement for base class (could override later in prey/predator class to give unique movement)
     def change_position(self):
         if not self._moving:
@@ -29,17 +30,18 @@ class MyBaseAgent(Agent):
 
         if random.random() < 0.1:
             self.direction = Vector2(
-                random.uniform(self.direction.x - 0.35, self.direction.x + 0.35), 
+                random.uniform(self.direction.x - 0.35, self.direction.x + 0.35),
                 random.uniform(self.direction.y - 0.35, self.direction.y + 0.35)
-                )
+            )
             self.direction = self.direction.normalize()
 
         self.move = self.direction
         self.pos += self.move
-    
+
 
 class Prey(MyBaseAgent):
-    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None, move: Vector2 | None = None):
+    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None,
+                 move: Vector2 | None = None):
         super().__init__(images, simulation, pos, move)
         self.simulation = simulation
 
@@ -47,13 +49,14 @@ class Prey(MyBaseAgent):
         self.save_data('type', 'Prey')
 
         # Only perform actions every timestep : random chance to reproduce
-        if self.shared.counter % TIMESTEP_INTERVAL == 0 and random.random() < 0.008: # 0.008
+        if self.shared.counter % TIMESTEP_INTERVAL == 0 and random.random() < 0.006:  # 0.008
             self.reproduce()
             # print("split")
 
 
 class Predator(MyBaseAgent):
-    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None, move: Vector2 | None = None):
+    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None,
+                 move: Vector2 | None = None):
         super().__init__(images, simulation, pos, move)
         self.simulation = simulation
         self.ate = -float('inf')
@@ -64,7 +67,7 @@ class Predator(MyBaseAgent):
         # Only perform actions every timestep
         if self.shared.counter % TIMESTEP_INTERVAL == 0:
             # If predator ate more than 45 frames ago, let it eat again
-            if self.shared.counter > self.ate + 30: # 30
+            if self.shared.counter > self.ate + 30:  # 30
                 self.ate = -float('inf')
 
             neighbors = self.in_proximity_accuracy()
@@ -75,17 +78,18 @@ class Predator(MyBaseAgent):
                     self.ate = self.shared.counter
 
                     # Chance to spawn another predator after eating
-                    if random.random() < 0.374: # 0.374
+                    if random.random() < 0.38:  # 0.374
                         self.reproduce()
                         # print("ate a mf")
 
             # Chance to die randomly
-            if random.random() < 0.0088: # 0.0088
+            if random.random() < 0.0088:  # 0.0088
                 self.kill()
 
 
 class PredatorWithEnergy(Predator):
-    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None, move: Vector2 | None = None):
+    def __init__(self, images: list[Surface], simulation: MySimulation, pos: Vector2 | None = None,
+                 move: Vector2 | None = None):
         super().__init__(images, simulation, pos, move)
         self.energy = 100
 
@@ -99,7 +103,7 @@ class PredatorWithEnergy(Predator):
                 self.kill()
 
             # If predator ate more than some amount of frames ago, let it eat again
-            if self.shared.counter > self.ate + 20:
+            if self.shared.counter > self.ate + 15:
                 self.ate = -float('inf')
 
             neighbors = self.in_proximity_accuracy()
@@ -111,19 +115,19 @@ class PredatorWithEnergy(Predator):
                     self.ate = self.shared.counter
 
                     # Chance to spawn another predator after eating
-                    if random.random() < 0.01:
+                    if random.random() < 0.045:
                         self.reproduce()
                         # print("ate a mf")
 
             # Chance to die randomly
             if random.random() < self._calculate_death_probability(self.energy):
                 self.kill()
-    
+
     def _calculate_death_probability(self, energy: float) -> float:
         a = 0.4
         prob = math.e ** (-a * energy)
         return prob
-    
+
     def change_position(self):
         if not self._moving:
             return
@@ -141,27 +145,26 @@ class PredatorWithEnergy(Predator):
         else:
             if random.random() < 0.01:
                 self.direction = Vector2(
-                    random.uniform(self.direction.x - 0.35, self.direction.x + 0.35), 
+                    random.uniform(self.direction.x - 0.35, self.direction.x + 0.35),
                     random.uniform(self.direction.y - 0.35, self.direction.y + 0.35)
-                    )
+                )
                 self.direction = self.direction.normalize()
 
         speed = max(0.7, self.energy / 75)
-        
+
         self.move = self.direction * speed
         self.pos += self.move
 
     def _get_closest_target(self, neighbors: ProximityIter) -> Tuple[bool, Prey]:
         closestDist = float('inf')
         closestTarget = None
-        
+
         for agent, distance in neighbors:
             if isinstance(agent, Prey) and distance < closestDist:
                 closestDist = distance
                 closestTarget = agent
-        
+
         if closestTarget != None:
             return True, closestTarget
-        
-        return False, None
 
+        return False, None
